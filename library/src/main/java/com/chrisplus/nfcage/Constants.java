f3ecfe7cf664f8c6d4a0b99f46e6d96edb875cf2
Shiqi Jiang <chrisplus.jiang@gmail.com>
2016-07-10 16:45:08 +0800

    ADD inner attr

--- library/src/main/java/com/chrisplus/nfcage/Constants.java
+++ library/src/main/java/com/chrisplus/nfcage/Constants.java
@@ -19,4 +19,21 @@ package com.chrisplus.nfcage;
  */
 public class Constants {
     public static final String TAG = Constants.class.getSimpleName();
+
+    public static class JsonInnerAttr{
+        public static final String NAME = "name";
+        public static final String ISSUER = "issuer";
+        public static final String PROTOCOL = "protocol";
+        public static final String APPLICATIONS = "applications";
+        public static final String BALANCE = "balance";
+        public static final String TRANSACTION = "transaction";
+        public static final String RECORD = "record";
+        public static final String ID = "id";
+        public static final String SFI = "sfi";
+        public static final String LOC = "loc";
+        public static final String LEN = "len";
+        public static final String TYPE = "type";
+        public static final String ENDIAN = "endian";
+        public static final String COUNT = "count";
+    }
 }
